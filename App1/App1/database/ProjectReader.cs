fd07aa436134a2ac9cf9b1ccb52993f2a7587352
Vincent van den Broek <vavandenbroek@gmail.com>
2017-12-06 21:38:13 +0100

    Made it working, changed name.

--- App1/App1/database/ProjectReader.cs
+++ App1/App1/database/ProjectReader.cs
@@ -1,64 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 
-namespace App1.database
-{
-    /// <summary>
-    /// A static class that can read and write to local Project and Activity storage.
-    /// This class is currently a stub.
-    /// </summary>
-    public static class ProjectReader
-    {
-        //these values are for testing, and should be removed when actual work on this class begins.
-        private static IList<Project> localProjects = new List<Project>(); // all local projects on this machine
+//namespace App1.database
+//{
+//    /// <summary>
+//    /// A static class that can read and write to local Project and Activity storage.
+//    /// This class is currently a stub.
+//    /// </summary>
+//    public static class ProjectReader
+//    {
+//        //these values are for testing, and should be removed when actual work on this class begins.
+//        private static IList<Project> localProjects = new List<Project>(); // all local projects on this machine
 
-        private static IList<Project> localNewProjects = new List<Project>(); // only new projects on this machine, used to write to database
-        public static IList<Project> GetProjects()
-        {
-            return localProjects;
-        }
+//        private static IList<Project> localNewProjects = new List<Project>(); // only new projects on this machine, used to write to database
+//        public static IList<Project> GetProjects()
+//        {
+//            return localProjects;
+//        }
 
-        public static void AddProject(Project project)
-        {
-            localProjects.Add(project);
-            localNewProjects.Add(project);
-            return;
-        }
+//        public static void AddProject(Project project)
+//        {
+//            localProjects.Add(project);
+//            localNewProjects.Add(project);
+//            return;
+//        }
 
-        public static void AddActivity(Project project, String activity)
-        {
-            project.ActivityList.Add(activity);
-        }
+//        public static void AddActivity(Project project, String activity)
+//        {
+//            project.ActivityList.Add(activity);
+//        }
 
-        public static Project ProjectFromName(String name)
-        {
-            foreach(Project p in localProjects)
-            {
-                if (name.Equals(p.Name))
-                {
-                    return p;
-                }
-            }
-            return null;
-        }
+//        public static Project ProjectFromName(String name)
+//        {
+//            foreach(Project p in localProjects)
+//            {
+//                if (name.Equals(p.Name))
+//                {
+//                    return p;
+//                }
+//            }
+//            return null;
+//        }
 
-        public static void ReadFromDatabase()
-        {
-            //TODO
-        }
+//        public static void ReadFromDatabase()
+//        {
+//            //TODO
+//        }
 
-        public static void WriteToDatabase()
-        {
-            //TODO
+//        public static void WriteToDatabase()
+//        {
+//            //TODO
 
 
 
-            localNewProjects.Clear();
-            return;
-        }
-    }
-}
+//            localNewProjects.Clear();
+//            return;
+//        }
+//    }
+//}
--- App1/App1/database/SzaboQL.cs
+++ App1/App1/database/SzaboQL.cs
@@ -1,133 +1,133 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SQLite;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using SQLite;
 
 
-namespace App1.database
-{
-    class SzaboQL
-    {
-        public SzaboQL()
-        { }
+//namespace App1.database
+//{
+//    class SzaboQL
+//    {
+//        public SzaboQL()
+//        { }
 
-        SQLiteConnection sqlite_conn;
-        SQLiteCommand sqlite_cmd;
+//        SQLiteConnection sqlite_conn;
+//        SQLiteCommand sqlite_cmd;
         
 
-        public void LocalConnection(string database_name)
-        {
+//        public void LocalConnection(string database_name)
+//        {
 
 
-            string connstring = "Data Source=";
-            connstring += database_name;
-            if (connstring.Contains(".sqlite") == false)
-                connstring += ".sqlite;";
-            else
-                connstring += ";";
+//            string connstring = "Data Source=";
+//            connstring += database_name;
+//            if (connstring.Contains(".sqlite") == false)
+//                connstring += ".sqlite;";
+//            else
+//                connstring += ";";
 
-            connstring += "Version=3;";
-            sqlite_conn = new SQLiteConnection(connstring);
-            sqlite_conn.Open();
-            sqlite_cmd = sqlite_conn.CreateCommand();
-        }
+//            connstring += "Version=3;";
+//            sqlite_conn = new SQLiteConnection(connstring);
+//            sqlite_conn.Open();
+//            sqlite_cmd = sqlite_conn.CreateCommand();
+//        }
 
-        public void Insert(string Tabelnaam, string[] kolommen, string[] data)
-        {
+//        public void Insert(string Tabelnaam, string[] kolommen, string[] data)
+//        {
 
-            SQLiteCommand command = new SQLiteCommand();
-            string insertstring = "INSERT INTO " + Tabelnaam + " (";
+//            SQLiteCommand command = new SQLiteCommand();
+//            string insertstring = "INSERT INTO " + Tabelnaam + " (";
          
-            //Kijk of voor elke kolom een waarde is ingesteld
-            if (data.Length != kolommen.Length)
-            {
+//            //Kijk of voor elke kolom een waarde is ingesteld
+//            if (data.Length != kolommen.Length)
+//            {
 
-                //THROW EXCEPTION
+//                //THROW EXCEPTION
 
-            }
+//            }
 
-            for (int i = 0; i < kolommen.Length - 1; i++)
-            {
-                insertstring += kolommen[i] + ", ";
-            }
+//            for (int i = 0; i < kolommen.Length - 1; i++)
+//            {
+//                insertstring += kolommen[i] + ", ";
+//            }
 
-            insertstring += kolommen[kolommen.Length - 1] + ") ";
-            insertstring += "VALUES (";
+//            insertstring += kolommen[kolommen.Length - 1] + ") ";
+//            insertstring += "VALUES (";
 
-            for (int i = 0; i < data.Length - 1; i++)
-            {
+//            for (int i = 0; i < data.Length - 1; i++)
+//            {
 
-                    insertstring += IntOrString(data[i])+ ", ";           
-            }
+//                    insertstring += IntOrString(data[i])+ ", ";           
+//            }
             
-            insertstring += IntOrString(data[data.Length - 1]) + ");";
+//            insertstring += IntOrString(data[data.Length - 1]) + ");";
        
-            sqlite_cmd.CommandText = insertstring;
-            //sqlite_cmd.ExecuteNonQuery();
-        }
+//            sqlite_cmd.CommandText = insertstring;
+//            //sqlite_cmd.ExecuteNonQuery();
+//        }
 
 
-        public void update(string tabel,string aan_te_passen_kolom, string nieuwe_waarde, string kolom_voorwaarde, string waarde_voorwaarde)
-        {
-            string updatestring = "UPDATE " + tabel + " SET ";
-            updatestring += aan_te_passen_kolom + " =";
+//        public void update(string tabel,string aan_te_passen_kolom, string nieuwe_waarde, string kolom_voorwaarde, string waarde_voorwaarde)
+//        {
+//            string updatestring = "UPDATE " + tabel + " SET ";
+//            updatestring += aan_te_passen_kolom + " =";
 
-            updatestring += IntOrString(nieuwe_waarde);      
-            updatestring += " WHERE " + kolom_voorwaarde+ "=";
+//            updatestring += IntOrString(nieuwe_waarde);      
+//            updatestring += " WHERE " + kolom_voorwaarde+ "=";
                    
-            updatestring += IntOrString(waarde_voorwaarde);
+//            updatestring += IntOrString(waarde_voorwaarde);
                       
-            updatestring += ";";
-            sqlite_cmd.CommandText = updatestring;
-            //sqlite_cmd.ExecuteNonQuery();
-        }
+//            updatestring += ";";
+//            sqlite_cmd.CommandText = updatestring;
+//            //sqlite_cmd.ExecuteNonQuery();
+//        }
 
-        public void create(string tabelnaam, string[] kolomnamen, string[] types, string primary_key)
-        {
-            //ERROR CHECK STUKJE:
-            if (kolomnamen.Length != types.Length)
-                goto stop; //error: not all colums have a dedicate type or vice versa
-            if (Array.IndexOf(kolomnamen, primary_key) < 0 && primary_key != "") 
-                goto stop;  //error: primary key not in table
+//        public void create(string tabelnaam, string[] kolomnamen, string[] types, string primary_key)
+//        {
+//            //ERROR CHECK STUKJE:
+//            if (kolomnamen.Length != types.Length)
+//                goto stop; //error: not all colums have a dedicate type or vice versa
+//            if (Array.IndexOf(kolomnamen, primary_key) < 0 && primary_key != "") 
+//                goto stop;  //error: primary key not in table
 
-            string createstring = "CREATE TABLE " + tabelnaam + " (";
+//            string createstring = "CREATE TABLE " + tabelnaam + " (";
 
-            for(int i = 0; i < kolomnamen.Length - 1; i++)
-            {         
-                createstring += kolomnamen[i] +" " + types[i] + IsPrimary(kolomnamen[i],primary_key) +", ";
-            }
-            createstring += kolomnamen[kolomnamen.Length - 1] + " " + types[kolomnamen.Length - 1] + IsPrimary(kolomnamen[kolomnamen.Length - 1], primary_key) + ");";
+//            for(int i = 0; i < kolomnamen.Length - 1; i++)
+//            {         
+//                createstring += kolomnamen[i] +" " + types[i] + IsPrimary(kolomnamen[i],primary_key) +", ";
+//            }
+//            createstring += kolomnamen[kolomnamen.Length - 1] + " " + types[kolomnamen.Length - 1] + IsPrimary(kolomnamen[kolomnamen.Length - 1], primary_key) + ");";
 
-            sqlite_cmd.CommandText = createstring;
+//            sqlite_cmd.CommandText = createstring;
 
-            try { sqlite_cmd.ExecuteNonQuery(); }
-            catch { /*Tabel bestaat al */}
+//            try { sqlite_cmd.ExecuteNonQuery(); }
+//            catch { /*Tabel bestaat al */}
 
-                stop:;
-        }
+//                stop:;
+//        }
 
-        public void close()
-        {
-            sqlite_conn.Close();
-        }
+//        public void close()
+//        {
+//            sqlite_conn.Close();
+//        }
 
-        public string IsPrimary(string naam, string primary)
-        {
-            if (naam == primary)
-                return " PRIMARY KEY";
-            return " ";
-        }
+//        public string IsPrimary(string naam, string primary)
+//        {
+//            if (naam == primary)
+//                return " PRIMARY KEY";
+//            return " ";
+//        }
 
-        public string IntOrString(string input)
-        {
-            string result = "";
-            if (input.Contains("PARSE_INT"))
-                result += input.Replace("PARSE_INT:", "");
-            else
-                result += "'" + input + "'";
-            return result;
-        }
-    }
-}
+//        public string IntOrString(string input)
+//        {
+//            string result = "";
+//            if (input.Contains("PARSE_INT"))
+//                result += input.Replace("PARSE_INT:", "");
+//            else
+//                result += "'" + input + "'";
+//            return result;
+//        }
+//    }
+//}
